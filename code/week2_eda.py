8cc495430b82fbf10b6438c3490489a98d6ccfcc
erikat21 <ertong@ucsd.edu>
2026-06-29 13:53:23 -0700

    EDA for listings and sold datasets, identified potential outliers to remove and columns to remove

--- code/week2_eda.py
+++ code/week2_eda.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 listing_df = pd.read_csv("output/listing.csv")
 sold_df = pd.read_csv("output/sold.csv")
@@ -20,7 +21,7 @@ def missing_val_analysis(df, name, percent):
     })
     print(f"{name} null report: \n{null_report}")
     high_missing = null_report[null_report['Null Percentage'] > percent]
-    print(f"{name} has {len(high_missing)} columns that are more than 90% null. The columns are: \n{high_missing.round(2)}")
+    print(f"{name} has {len(high_missing)} columns that are more than {percent}% null. The columns are: \n{high_missing.round(2)}")
     return high_missing
 
 high_missing_listings = missing_val_analysis(listing_df, 'Listings Dataset', 90)
@@ -28,7 +29,29 @@ high_missing_sold = missing_val_analysis(sold_df, "Sold Dataset", 90)
 # consider dropping columns with more than 50% missing values unless they are core fields or seem like they could be important
 
 key_fields = ['ClosePrice', "ListPrice", "OriginalListPrice", "LivingArea", "LotSizeAcres", "BedroomsTotal", "BathroomsTotalInteger", "DaysOnMarket", "YearBuilt"]
+# for col in key_fields:
+#     plt.hist(sold_df[col].dropna(), bins = 50, color='steelblue')
+#     plt.title(f"Sold dataset {col} Histogram")
+#     plt.show()
 
+#     plt.boxplot(sold_df[col].dropna())
+#     plt.title(f"Sold dataset {col} Boxplot")
+#     plt.show()
+
+#     plt.hist(listing_df[col].dropna(), bins = 50, color='steelblue')
+#     plt.title(f"Listings dataset {col} Histogram")
+#     plt.show()
+
+#     plt.boxplot(listing_df[col].dropna())
+#     plt.title(f"Listings dataset {col} Boxplot")
+#     plt.show()
+
+custom_percentiles = [0.01, 0.05, 0.10, 0.25, 0.50, 0.75, 0.90, 0.95, 0.99]
+print(f" Sold dataset key fields percentile summary {sold_df[key_fields].describe(percentiles=custom_percentiles).T.round(2)}")
+print(f" Listings dataset key fields percentile summary {listing_df[key_fields].describe(percentiles=custom_percentiles).T.round(2)}")
+# need to drop rows with impossible values such as 0's and negatives, the houses cant be on market for 
+# negative days and the houses cant have 0 bedrooms etc, the 0 dollar close price could be from houses that 
+# havent sold yet so instead of NA its 0, might not need to drop those rows
 
 target_fields = ['ClosePrice', 'LivingArea', 'DaysOnMarket']
 listing_distribution_summary = listing_df[target_fields].describe().T
@@ -37,4 +60,11 @@ print(f"Listings dataset distribution summary: \n{listing_distribution_summary.r
 sold_distribution_summary = sold_df[target_fields].describe().T
 sold_distribution_summary.rename(columns={'50%': 'median'}, inplace= True)
 print(f"Sold dataset distribution summary: \n{sold_distribution_summary.round(2)}")
+# extreme outliers are most likely not wrong but could be from mansions or expensive areas so, 
+# we can keep them for now and maybe remove from analysis later so its not skewed since those properties are more rare,
+# the extremely low outliers are most likely wrong though since houses cannot be sold for 0 dollars so those will need to be filtered out
 
+
+missing_val_analysis(listing_df, 'Listings Dataset', 50)
+missing_val_analysis(sold_df, "Sold Dataset", 50)
+# can drop all of these columns I think, lots of null and they are not key to analysis most likely
